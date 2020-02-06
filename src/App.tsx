74a9aee95818769a645f4c570c26e6ea3ddc6ba1
karen-ifrt <55798470+karen-ifrt@users.noreply.github.com>
2020-02-06 16:40:56 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import Pokemon from './models/pokemon';
+import POKEMONS from './models/mock-pokemon';
+  
+const App: FunctionComponent = () => {
+ const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+ useEffect(() => {
+     setPokemons(POKEMONS);
+ }, []);
+    
+ return (
+  <div>
+      <h1>Pokédex</h1>
+      <p>Il y a {pokemons.length} pokémons dans le Pokedex.</p>
+  </div>
+ )
+}
+  
+export default App;
