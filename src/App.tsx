449b8550344ef3dd2e186bb2b693678e747f6687
karen-ifrt <55798470+karen-ifrt@users.noreply.github.com>
2020-02-10 16:53:30 +0100

    add edit form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React, { FunctionComponent } from 'react';
 import PokemonList from './pages/pokemon-list';
+import PokemonDetail from './pages/pokemon-detail';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import PageNotFound from './pages/page-not-found';
+import PokemonEdit from './pages/pokemon-edit';
 
 const App: FunctionComponent = () => {
 
     return (
-        <PokemonList />
+        <Router>
+            <div>
+                {/* La barre de navigation commun à toutes les pages */}
+                <nav>
+                    <div className="nav-wrapper teal">
+                        <Link to="/" className="brand-logo center">Pokédex</Link>
+                    </div>
+                </nav>
+                {/* Le système de gestion des routes de notre application */}
+                <Switch>
+                    <Route exact path="/" component={PokemonList} />
+                    <Route exact path="/pokemons" component={PokemonList} />
+                    <Route exact path="/pokemons/edit/:id" component={PokemonEdit} />
+                    <Route path="/pokemons/:id" component={PokemonDetail} />
+                    <Route component={PageNotFound} />
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
